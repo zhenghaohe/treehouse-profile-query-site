87a8671b361451a61b12e43dd1e144f49f93f9ed
zhenghao <iron4wine@outlook.com>
2018-02-25 11:51:34 -0800

    set the port to a non fixed number

--- index.js
+++ index.js
@@ -3,6 +3,8 @@
 // Solution: Use Node.js to perform the profile lookups and serve our template via HTTP
 
 var router = require("./router.js");
+var port = process.env.PORT || 1337
+
 
 // Create a web server
 var http = require("http");
@@ -10,6 +12,6 @@ var http = require("http");
 http.createServer(function (request, response) {
   router.home(request, response);
   router.user(request, response);
-}).listen(1337);
+}).listen(port);
 
 console.log("Sever running at http://127.0.0.1:1337/");
